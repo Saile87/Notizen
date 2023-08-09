20ebd0f2f7be509adf7c3926731754d5d64c4949
Elias Breitenbach <elias.b@me.com>
2023-08-09 19:47:47 +0200

    Create a project in github

---
+++ Notizen/Notizen/NotizenApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotizenApp.swift
+//  Notizen
+//
+//  Created by Elias Breitenbach on 09.08.23.
+//
+
+import SwiftUI
+
+@main
+struct NotizenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
