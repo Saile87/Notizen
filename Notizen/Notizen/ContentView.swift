cf0e4c093d38969aca244f547d0136a6186f5d0f
Elias Breitenbach <elias.b@me.com>
2023-08-16 20:18:49 +0200

    Final

--- Notizen/Notizen/ContentView.swift
+++ Notizen/Notizen/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var notizen: [String] = ["Staub Saugen", "Einkaufen", "Aufräumen"]
+    @State private var notiz = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(notizen, id: \.self) { notiz in
+                        Text(notiz)
+                    }
+                    .onDelete(perform: { indexSet in
+                        notizen.remove(atOffsets: indexSet)
+                    })
+                }
+                .navigationTitle("Notizen")
+                HStack {
+                    TextField("Neue Notiz", text: $notiz)
+                    Button("Hinzufügen") {
+                        notizen.append(notiz)
+                        notiz = ""
+                    }
+                } .padding(.horizontal)
+            }
         }
-        .padding()
     }
 }
 
